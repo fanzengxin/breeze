5c63c4411cfff1e1ee2766c1449300b9e5fcee85
樊增鑫 <fanzx@yozosoft.com>
2022-05-16 10:37:50 +0800

    添加文件存储模块

--- breeze-core/src/main/java/org/breeze/core/storage/FileStorageInterface.java
+++ breeze-core/src/main/java/org/breeze/core/storage/FileStorageInterface.java
@@ -16,7 +16,34 @@ public interface FileStorageInterface {
      * 文件上传
      *
      * @param inputStream
-     * @return
+     * @return 文件路径
      */
     String upload(InputStream inputStream);
+
+    /**
+     * 文件下载
+     *
+     * @param filePath      文件路径
+     * @param dataBaseIndex 文件库
+     * @return 文件下载流
+     */
+    InputStream download(String filePath, String dataBaseIndex);
+
+    /**
+     * 文件删除
+     *
+     * @param filePath      文件路径
+     * @param dataBaseIndex 文件库
+     * @return 删除成功标记
+     */
+    boolean remove(String filePath, String dataBaseIndex);
+
+    /**
+     * 获取文件外部访问链接
+     *
+     * @param filePath      文件路径
+     * @param dataBaseIndex 文件库
+     * @return 文件访问地址
+     */
+    String getUrl(String filePath, String dataBaseIndex);
 }
